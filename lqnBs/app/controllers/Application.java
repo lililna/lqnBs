48c4a660987e428b066bc5092c642d151baa2a68
李巧娜 <1423716128@qq.com>
2017-03-20 20:34:17 +0800

    项目框架

---
+++ lqnBs/app/controllers/Application.java
@@ -0,0 +1,78 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import java.util.*;
+
+import models.*;
+
+public class Application extends Controller {
+
+    public static void index() {
+        render();
+    }
+    public static void bangzhu() {
+        render();
+    }
+    public static void daili() {
+        render();
+    }
+    public static void details() {
+        render();
+    }
+    public static void dingdan() {
+        render();
+    }
+    public static void dizhi() {
+        render();
+    }
+    public static void erweima() {
+        render();
+    }
+    public static void fenlei() {
+        render();
+    }
+    public static void gwc() {
+        render();
+    }
+    public static void huanying() {
+        render();
+    }
+    public static void jifen() {
+        render();
+    }
+    public static void naconfirm() {
+        render();
+    }
+    public static void shenqingtixian() {
+        render();
+    }
+    public static void splist() {
+        render();
+    }
+    public static void tuiguangdianji() {
+        render();
+    }
+    public static void tuiguangdingdan() {
+        render();
+    }
+    public static void tuiguangguanzhu() {
+        render();
+    }
+    public static void wodeyinhangka() {
+        render();
+    }
+    public static void wodeyongjin() {
+        render();
+    }
+    public static void yongjinmingxi() {
+        render();
+    }
+    public static void ziliao() {
+        render();
+    }
+    public static void ZX() {
+        render();
+    }
+}
